b77e53a52a45776838997292bd119f50b19c556b
riyagoyal01 <dr33242@gmail.com>
2026-03-19 13:56:06 +0530

    updated scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -2,8 +2,6 @@ import pytesseract
 
 def scan_image(image):
 
-    pytesseract.pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
-
     text = pytesseract.image_to_string(image)
 
     return text
